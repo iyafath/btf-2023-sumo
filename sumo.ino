7b36ca06d01351f86de14f0488004ba02806ecf9
iyafath <124046371+iyafath@users.noreply.github.com>
2023-11-25 16:07:33 +0700

    Update sumo.ino
    fixed notes on debug/motor mode
    fixed response delay of spinR and spinL

--- sumo.ino
+++ sumo.ino
@@ -12,10 +12,10 @@ Robot abilities:
 
 */
 
-// set to FALSE to disable serial output
-#define debug TRUE
+// comment out to disable serial output
+// #define debug TRUE
 
-// set to FALSE to disable motor control
+// comment out to disable motor control
 #define motor TRUE
 
 #include <PS4Controller.h>
@@ -25,9 +25,9 @@ Robot abilities:
 #include "esp_err.h"
 
 // board and pin definitions
-const char ESP_ADDRESS[] = "00:00:00:00:00:00";
+const char ESP_ADDRESS[] = "C8:F0:9E:F2:30:D4";
 const int PIN_R_SPD = 25, PIN_R_FWD = 32, PIN_R_BWD = 33;
-const int PIN_L_SPD = 0, PIN_L_FWD = 0, PIN_L_BWD = 0;
+const int PIN_L_SPD = 18, PIN_L_FWD = 19, PIN_L_BWD = 21;
 const int STICK_RECONNECT_DELAY = 3000; // if ps4 gets disconnected, keep trying to reconnect with this cooldown between attempts
 unsigned long stickReconnectTimer = 0;
 
@@ -36,7 +36,8 @@ struct Controller {
   // x, sq, o, tr -> cross, square, circle, triangle
   int rx; // rotate
   int ly; // forward/backward
-  bool x; // "dash", temp speed boost, prevents rotation, has cooldown
+  bool x; // "dashF", temp speed boost, prevents rotation, has cooldown
+  bool sq; // "dashB"
   bool r1, l1; // fast 180 deg turn, prevents forward/backward, has cooldown
 } stick;
 
@@ -44,12 +45,12 @@ struct Controller {
 // necessary for superposition: speed_max + rot_max < stick_max
 // rot lead & trail = percentage of rot speed to be applied for fw wheel and bw wheel
 const int STICK_ZERO = 10, STICK_FINE = 100, STICK_MAX = 128;
-const int SPEED_MIN = 15, SPEED_MID = 23, SPEED_MAX = 70;
-const int ROT_MIN = 15, ROT_MID = 20, ROT_MAX = 30;
+const int SPEED_MIN = 15, SPEED_MID = 40, SPEED_MAX = 70;
+const int ROT_MIN = 15, ROT_MID = 35, ROT_MAX = 57;
 const float ROT_LEAD = 1, ROT_TRAIL = 1;
 
 // special actions processing
-enum Action {Neutral, Dash, SpinR, SpinL};
+enum Action {Neutral, DashF, DashB, SpinR, SpinL};
 const int DASH_DURATION = 300, SPIN_DURATION = 300; // time it takes for an action to finish
 const int SPIN_ACTIVE = 100; // how long motor should stay on during spin
 const float DASH_SPEED = 1, SPIN_SPEED = 1; // normalized speed
@@ -60,7 +61,8 @@ unsigned long lastActionStart = -100000;
 const char* getActionName(Action act) {
   switch(act) {
     case Neutral: return "Neutral";
-    case Dash: return "Dash";
+    case DashF: return "DashF";
+    case DashB: return "DashB";
     case SpinR: return "SpinR";
     case SpinL: return "SpinR";
   }
@@ -159,6 +161,7 @@ void setSpeed(float l, float r) {
   else {
     Serial.printf("L+%d\t", (int)(l * 255));
   }
+  Serial.println();
 }
 
 #endif
@@ -211,21 +214,26 @@ void move(int speedStick, int rotStick) {
 
 // ======================== ACTIONS ============================
 
-void dash()
+void dashF()
 {
   setSpeed(DASH_SPEED, DASH_SPEED);
+}
+
+void dashB()
+{
+  setSpeed(-DASH_SPEED, -DASH_SPEED);
 }
 
 void spinR()
 {
   // active motor phase
-  if (checkDebounce(100)) setSpeed(SPIN_SPEED * ROT_LEAD, -SPIN_SPEED * ROT_TRAIL);
+  if (!checkDebounce(SPIN_ACTIVE)) setSpeed(SPIN_SPEED * ROT_LEAD, -SPIN_SPEED * ROT_TRAIL);
   else setSpeed(0, 0);
 }
 
 void spinL()
 {
-  if (checkDebounce(100)) setSpeed(-SPIN_SPEED * ROT_TRAIL, SPIN_SPEED * ROT_LEAD);
+  if (!checkDebounce(SPIN_ACTIVE)) setSpeed(-SPIN_SPEED * ROT_TRAIL, SPIN_SPEED * ROT_LEAD);
   else setSpeed(0, 0);
 }
 
@@ -237,7 +245,7 @@ void reset()
 // ======================== PROCESS ACTIONS ============================
 
 unsigned long getCooldown (Action act) {
-  if (act == Dash) return DASH_DURATION;
+  if (act == DashF || act == DashB) return DASH_DURATION;
   else if (act != Neutral) return SPIN_DURATION;
   return 0;
 }
@@ -283,7 +291,8 @@ int processActionQueue()
 
 void executeAction()
 {
-  if (currentAction == Dash) dash();
+  if (currentAction == DashF) dashF();
+  else if(currentAction == DashB) dashB();
   else if (currentAction == SpinR) spinR();
   else if (currentAction == SpinL) spinL();
 }
@@ -293,8 +302,10 @@ void processController(int actionQueueStatus)
   // spin
   stick.r1 = PS4.R1();
   stick.l1 = PS4.L1();
-  // dash
+  // dashF
   stick.x = PS4.Cross();
+  // dashB
+  stick.sq = PS4.Square();
   // analog sticks
   stick.ly = PS4.LStickY();
   stick.rx = PS4.RStickX();
@@ -303,7 +314,10 @@ void processController(int actionQueueStatus)
   if (actionQueueStatus != 2) {
     // check action validity, prioritize dash
     if (stick.x) {
-      if (currentAction != Dash || checkDebounce()) nextAction = Dash;
+      if (currentAction != DashF || checkDebounce()) nextAction = DashF;
+    }
+    else if(stick.sq) {
+        if (currentAction != DashB || checkDebounce()) nextAction = DashB;
     }
     else if (stick.r1) {
       if (currentAction != SpinR || checkDebounce()) nextAction = SpinR;
@@ -317,9 +331,10 @@ void processController(int actionQueueStatus)
   if (actionQueueStatus == 0) move(stick.ly, stick.rx);
 
 #ifdef debug
-  if (stick.r1 || stick.l1 || stick.x || stick.ly || stick.rx) {
+  if (stick.r1 || stick.l1 || stick.x || stick.sq || stick.ly || stick.rx) {
     Serial.print("READINGS\t");
     if (stick.x) Serial.print(" X");
+    if (stick.sq) Serial.print(" Square");
     if (stick.r1) Serial.print(" R1");
     if (stick.l1) Serial.print(" L1");
     if (stick.ly) {
@@ -328,8 +343,8 @@ void processController(int actionQueueStatus)
     if (stick.rx) {
       Serial.print(" RX:"); Serial.print(stick.ly);
     }
-    Serial.println();
   }
+  Serial.println();
 #endif
 }
 
@@ -343,12 +358,10 @@ void setup() {
 
   PS4.begin(ESP_ADDRESS);
   PS4.attachOnConnect(onConnect);
-  PS4.attachOnDisconnect(onDisconnect);
+  PS4.attachOnDisconnect(onDisconnect);   
 
-  #ifdef debug
   Serial.begin(115200);
   Serial.println();
-  #endif
 
   currentAction = nextAction = Neutral;
   reset();
@@ -361,9 +374,7 @@ void loop() {
     executeAction();
     
     processController(actionQueueStatus);
-#ifdef debug
     delay(50);
-#endif
 #ifndef motor
     delay(200);
 #endif
